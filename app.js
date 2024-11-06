2e2e1047ed783d05b19e39522c2bc87b4e7ec315
semiseoo <sameeha.imtiaz@ontariotechu.net>
2024-11-05 22:45:48 -0500

    removed || 10000

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ app.use(function(req, res, next){
     res.status(404).send("Can't find the page you're looking for, Sorry!");
 });
 
-const port = process.env.PORT || 10000;
+const port = process.env.PORT;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
 
 module.exports = app;
